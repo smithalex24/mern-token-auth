a15ed88c0507b7ff4b9307a5c0078d3249e9281c
smithalex24 <smith25243@gmail.com>
2018-05-08 10:15:05 -0700

    Nabar is looking great

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Router>
           <div className = "container">
             <Nav />
-            <p>Navbar coming soon</p>
             <Route exact path = "/" component = {Home} />
 
 
--- client/src/layout/Nav.js
+++ client/src/layout/Nav.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
 
 class Nav extends Component {
 	render() {
 		return (
-			<p>NAV BAR</p>
+			<div>
+				<nav className = "nav">
+					<Link to = "/">Home</Link>
+				</nav>
+				<header className="App-header">
+          			<img src={logo} className="App-logo" alt="logo" />
+          			<h1 className="App-title">Welcome to React</h1>
+        		</header>
+			</div>
 	
 	
 		);
